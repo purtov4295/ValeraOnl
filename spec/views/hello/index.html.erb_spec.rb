47d60f0c201244e65ac6411ce32a5241b6de6550
prtov4295 <purtov4295@gmail.com>
2022-04-29 05:08:47 +0700

    Rebase vievs

---
+++ spec/views/hello/index.html.erb_spec.rb
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+RSpec.describe 'hello/index.html.erb', type: :view do
+  it 'renders "Hello World"' do
+    render
+    expect(rendered).to match(/Hello World/)
+  end
+end
